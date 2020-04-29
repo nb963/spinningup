fbbede95e3ed277fe93a1378b60c7780540c5633
tanmayshankar <tanmayshankar@github.com>
2020-04-29 16:12:13 -0400

    fixing log probability.

--- spinup/algos/pytorch/ppo/core.py
+++ spinup/algos/pytorch/ppo/core.py
@@ -200,7 +200,8 @@ class HierarchicalActorCritic(nn.Module):
         latent_b_logprobabilitity = self.latent_b_policy._log_prob_from_distribution(latent_b_policy_distribution, batched_latent_b)
         latent_z_logprobabilitity = self.latent_z_policy._log_prob_from_distribution(latent_z_policy_distribution, batched_latent_z)
 
-        total_logprobabilities = logp_a.numpy() + latent_b_logprobabilitity.sum().numpy() + latent_z_logprobabilitity.sum().numpy()
+        total_logprobabilities = logp_a + latent_b_logprobabilitity + latent_z_logprobabilitity
+        embed()
 
         return pi, latent_b_policy_distribution, latent_z_policy_distribution, total_logprobabilities
 
