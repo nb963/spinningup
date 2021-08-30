77aa3efe870726afd3a086e93eec56fd091a517d
tanmayshankar <tanmay.shankar@gmail.com>
2021-08-30 13:49:54 -0400

    import

--- spinup/algos/pytorch/ppo/new_hierarchical_ppo.py
+++ spinup/algos/pytorch/ppo/new_hierarchical_ppo.py
@@ -1,8 +1,6 @@
 import numpy as np
-import torch
+import torch, os, gym, time
 from torch.optim import Adam
-import gym
-import time
 import spinup.algos.pytorch.ppo.core as core
 from spinup.utils.logx import EpochLogger
 from spinup.utils.mpi_pytorch import setup_pytorch_for_mpi, sync_params, mpi_avg_grads
